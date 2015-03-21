cb171919350f897cc525e2539ea5e311aec2f723
rlktradewright <rlking@aultan.com>
2015-03-21 00:23:55 +0000

    End of day commit

---
+++ GenerateManifest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TradeWright.ManifestUtilities;
+
+namespace TradeWright.GenerateManifest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gen = new ManifestGenerator();
+            Console.WriteLine(gen.Generate(args[0], true));
+        }
+    }
+}
